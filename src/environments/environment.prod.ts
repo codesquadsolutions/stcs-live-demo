fc51713dfd5e682e99b1cc8debcaad7bfc6b7247
codesquadsolutions <codesquadsolutions@gmail.com>
2023-01-30 15:25:31 +0530

    firebase credentials updtaed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBofed2IEikvVkTDUWWWbrFstcgdKbFl-0",
-    authDomain: "stcs-live.firebaseapp.com",
-    projectId: "stcs-live",
-    storageBucket: "stcs-live.appspot.com",
-    messagingSenderId: "753727762963",
-    appId: "1:753727762963:web:02fa86e79b189c641f45e6",
-    measurementId: "G-BY518K7C5M"
+    apiKey: "AIzaSyC0g7JK-vyF2H-tnhI9itqkOERljmoVV20",
+  authDomain: "stcs-live-demo.firebaseapp.com",
+  databaseURL: "https://stcs-live-demo-default-rtdb.firebaseio.com",
+  projectId: "stcs-live-demo",
+  storageBucket: "stcs-live-demo.appspot.com",
+  messagingSenderId: "879391157076",
+  appId: "1:879391157076:web:8bd49184dcab90d01fb6c1",
+  measurementId: "G-TX7E0PXP6D"
   }
 };
